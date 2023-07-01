92d7cdcb922c150b4c1de8cc23442b16312a7659
FedericoPacheco <fedepacheco2112@gmail.com>
2023-07-01 15:18:30 -0300

    Crear menu funcional

--- vsoa_spread_usr.c
+++ vsoa_spread_usr.c
@@ -3,122 +3,118 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-static	char	User[80];
-static  char    Spread_name[80];
-static  char    Private_group[MAX_GROUP_NAME];
-static  mailbox Mbox;
+#include <stdbool.h>
 
 static	int	Read_message();
-static	void	Usage( int argc, char *argv[] );
+static	void	Usage(int argc, char* argv[]);
 
-int main( int argc, char *argv[] )
+// ---------------------------------
+// SPREAD
+char usr[80];
+char nombre_spread[80];
+char nombre_grupo[MAX_GROUP_NAME];
+mailbox buzon;
+
+// ---------------------------------
+// Menu
+void imprimir_menu();
+#define OPC_UNIRSE_GRUPO 'j'
+#define OPC_SALIR_GRUPO  'l'
+#define OPC_ENVIAR_MSG   's'
+#define OPC_RECIBIR_MSG  'r'
+#define OPC_SALIR		 'q'
+
+bool validar_tipo_msg(char* tipo_msg);
+#define UNREL_MSG_STR "rel"
+#define REL_MSG_STR "unrel"
+#define FIFO_MSG_STR "fifo"
+#define CAUSAL_MSG_STR "causal"
+#define AGREED_MSG_STR "atom"
+#define SAFE_MSG_STR "safe"
+
+// ---------------------------------
+int main(int argc, char* argv[])
 {
-	int	ret;
-	unsigned int	mess_len;
-	char	mess[200];
+	char opc;
+	char tipo_msg[8];
 
-	/* connecting to the daemon, requesting group information */
-	ret = SP_connect( Spread_name, User, 0, 1, &Mbox, Private_group );
-	if( ret < 0 ) 
+	imprimir_menu();
+	do
 	{
-		SP_error( ret );
-		exit(0);
-	}
+		printf("\nOpcion: ");
+		scanf(" %c", &opc);
+		switch (opc)
+		{
+			case OPC_UNIRSE_GRUPO:
+				printf("Nombre del grupo: ");
+				scanf("%s", nombre_grupo);
+				// TO DO: unirse a grupo con spread
+				break;
+			case OPC_SALIR_GRUPO:
+				printf("Nombre del grupo: ");
+				scanf("%s", nombre_grupo);
+				// TO DO: salir de grupo con spread
+				break;
+			case OPC_ENVIAR_MSG:
+				printf("Tipo de mensaje (%s, %s, %s, %s, %s, %s): ", UNREL_MSG_STR, REL_MSG_STR, FIFO_MSG_STR, CAUSAL_MSG_STR, AGREED_MSG_STR, SAFE_MSG_STR);
+				scanf("%s", tipo_msg);
+				if (validar_tipo_msg(tipo_msg))
+				{
+					// TO DO: enviar mensaje con spread
+				}
+				else
+					printf("Error: el tipo de mensaje especificado no es valido\n");
+				break;
+			case OPC_RECIBIR_MSG:
+				// TO DO: recibir mensaje con spread
+				break;
+			case OPC_SALIR:
+				printf("Saliendo...\n");
+				break;
+			default:
+				//printf("%c\n", opc);
+				printf("Error: la opcion especificada no es valida\n\n");
+				break;
+		}
 
-	/* joining a group */
-	SP_join( Mbox, "simple_group" );
+	} while (opc != OPC_SALIR);
 
-	printf("enter message: ");
-	ret = (int) fgets( mess, 200, stdin );
-	if( ret==0 ) exit(0);
-	mess_len = strlen(mess);
-
-	/* multicast a message to that group */
-	ret = SP_multicast( Mbox, AGREED_MESS, "simple_group", 1, mess_len, mess );
-
-	/* 
-	 * reading a message.
-	 * Note that the first message will be the membreship caused by
-	 * the join (if group information was requested at connect time).
-	 * If that was the case, we read another message.
-	 */
-	do{
-		ret = Read_message();
-	} while( !Is_regular_mess( ret ) );
-
-	return 0;
+	return EXIT_SUCCESS;
 }
 
-static	int	Read_message()
+void imprimir_menu()
 {
-
-static	char		mess[102400];
-	char		sender[MAX_GROUP_NAME];
-	char		target_groups[100][MAX_GROUP_NAME];
-	int		num_groups;
-        membership_info memb_info;
-	int		service_type;
-	int16		mess_type;
-	int		endian_mismatch;
-	int		i;
-	int		ret;
-
-	printf("\n============================\n");
-        service_type = 0;
-	ret = SP_receive( Mbox, &service_type, sender, 100, &num_groups, target_groups, 
-		&mess_type, &endian_mismatch, sizeof(mess), mess );
-	if( ret < 0 ) 
-	{
-		SP_error( ret );
-		exit(0);
-	}
-
-	if( Is_regular_mess( service_type ) )
-	{
-		/* A regular message, sent by one of the processes */
-		mess[ret] = 0;
-		if     ( Is_unreliable_mess( service_type ) ) printf("received UNRELIABLE ");
-		else if( Is_reliable_mess(   service_type ) ) printf("received RELIABLE ");
-		else if( Is_fifo_mess(       service_type ) ) printf("received FIFO ");
-		else if( Is_causal_mess(     service_type ) ) printf("received CAUSAL ");
-		else if( Is_agreed_mess(     service_type ) ) printf("received AGREED ");
-		else if( Is_safe_mess(       service_type ) ) printf("received SAFE ");
-		printf("message from %s of type %d (endian %d), to %d groups \n(%d bytes): %s\n",
-			sender, mess_type, endian_mismatch, num_groups, ret, mess );
-
-	}
-	else if( Is_membership_mess( service_type ) )
-	{
-		/* A membership notification */
-		ret = SP_get_memb_info( mess, service_type, &memb_info );
-		if (ret < 0) {
-				printf("BUG: membership message does not have valid body\n");
-				SP_error( ret );
-				exit( 1 );
-		}
-		if ( Is_reg_memb_mess( service_type ) )
-		{
-			printf("received REGULAR membership ");
-			if( Is_caused_join_mess( service_type ) ) printf("caused by JOIN ");
-			if( Is_caused_leave_mess( service_type ) ) printf("caused by LEAVE ");
-			if( Is_caused_disconnect_mess( service_type ) ) printf("caused by DISCONNECT ");
-			printf("for group %s with %d members:\n",
-				sender, num_groups );
-			for( i=0; i < num_groups; i++ )
-				printf("\t%s\n", &target_groups[i][0] );
-			printf("grp id is %d %d %d\n",memb_info.gid.id[0], memb_info.gid.id[1], memb_info.gid.id[2] );
-		}
-		else if( Is_transition_mess(   service_type ) ) 
-		{
-			printf("received TRANSITIONAL membership for group %s\n", sender );
-		}
-		else if( Is_caused_leave_mess( service_type ) )
-		{
-			printf("received membership message that left group %s\n", sender );
-		}else printf("received incorrect membership message of type %d\n", service_type );
-	}
-	else 
-		printf("received message of unknown message type %d with %d bytes\n", service_type, ret);
-	return( service_type );
+	printf("------------------------------------------------\n");
+	printf("|               VSOA SPREAD USR                |\n");
+	printf("------------------------------------------------\n");
+	printf("|                                              |\n");
+	printf("|            %c - unirse a un grupo             |\n", OPC_UNIRSE_GRUPO);
+	printf("|            %c - salir de un grupo             |\n", OPC_SALIR_GRUPO);
+	printf("|            %c - enviar mensaje                |\n", OPC_ENVIAR_MSG);
+	printf("|            %c - recibir mensaje               |\n", OPC_RECIBIR_MSG);
+	printf("|                                              |\n");
+	printf("|            %c - salir                         |\n", OPC_SALIR);
+	printf("------------------------------------------------\n");
 }
+
+bool validar_tipo_msg(char* tipo_msg)
+{
+	if (strcmp(tipo_msg, UNREL_MSG_STR) == 0)
+		return true;
+	else if (strcmp(tipo_msg, REL_MSG_STR) == 0)
+		return true;
+	else if (strcmp(tipo_msg, FIFO_MSG_STR) == 0)
+		return true;
+	else if (strcmp(tipo_msg, CAUSAL_MSG_STR) == 0)
+		return true;
+	else if (strcmp(tipo_msg, AGREED_MSG_STR) == 0)
+		return true;
+	else if (strcmp(tipo_msg, SAFE_MSG_STR) == 0)
+		return true;
+	else
+		return false;
+}
+
+
+
+
