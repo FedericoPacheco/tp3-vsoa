44d1eb7ddd7242b28035314ab040fcff0b441d4f
FedericoPacheco <fedepacheco2112@gmail.com>
2023-07-08 17:21:07 -0300

    Agregar archivos de spread, modificar comando gcc

--- enviar_programa.sh
+++ enviar_programa.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo gcc -o vsoa_spread_usr vsoa_spread_usr.c -I/mnt/hgfs/TP3/Material/SPREAD/includeSpread -L/mnt/hgfs/TP3/Material/SPREAD/libSpread -lspread
+sudo gcc -o vsoa_spread_usr vsoa_spread_usr.c -I./SPREAD/include -L./SPREAD/lib -lspread
+
 scp vsoa_spread_usr ubuntuadm@10.62.245.196:/home/ubuntuadm
 scp vsoa_spread_usr ubuntuadm@10.62.245.189:/home/ubuntuadm
 scp vsoa_spread_usr ubuntuadm@10.62.245.123:/home/ubuntuadm
